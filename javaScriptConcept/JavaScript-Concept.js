de9c1bcbc5bbdc14e59eff6fce8b4c67f82da58f
Avinash Singh <avinash.singh@eka1.com>
2021-11-04 01:06:59 +0530

    Started Learning

---
+++ javaScriptConcept/JavaScript-Concept.js
@@ -0,0 +1,26 @@
+/* 
+polyfill for bind method
+
+*/
+
+let n = {
+  firstName: "John",
+  lastName: "S",
+};
+
+function printDetails() {
+  console.log(this.firstName + " " + this.lastName);
+}
+
+// let printName = printDetails.bind(n);
+// printName();
+
+Function.prototype.mybind = function (...args) {
+  let fn = this;
+  let params = args.slice(1);
+  return function (...args2) {
+    fn.apply(args[0], [...params, ...args2]);
+  };
+};
+
+let printName = printDetails.mybind(n)();
